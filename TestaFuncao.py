d3408801d9406b0cf805966c4864adbd6b9bd845
ednaldo17 <filhoednaldo07@gmail.com>
2025-07-03 20:06:46 -0300

    Testa Função V1

--- TestaFuncao.py
+++ TestaFuncao.py
@@ -3,3 +3,15 @@ import math
 def calcula_area_circulo(raio):
     area_circulo = math.pi * raio ** 2
     return round(area_circulo, 2)
+
+##################################################
+
+def calcula_area_triangulo(base, altura):
+    area_triangulo = (base * altura) / 2
+    return round(area_triangulo, 2)
+
+##################################################
+
+def calcula_area_retangulo(base, altura):
+    return base * altura
+
